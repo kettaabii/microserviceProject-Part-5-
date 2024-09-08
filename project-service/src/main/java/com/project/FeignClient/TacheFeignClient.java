29d6df5c22c2028ed7e616d3f4067afd76acc614
kettaabii <elkettabimohammedamin@gmail.com>
2024-09-08 22:59:37 +0100

    Fault Telerance using circuit breaker

--- project-service/src/main/java/com/project/FeignClient/TacheFeignClient.java
+++ project-service/src/main/java/com/project/FeignClient/TacheFeignClient.java
@@ -3,6 +3,7 @@ package com.project.FeignClient;
 
 import com.project.FallBack.TacheFeignClientFallback;
 import com.project.model.Tache;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -12,12 +13,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name="TACHESERVICE",fallback = TacheFeignClientFallback.class)
+@FeignClient(name="TACHESERVICE"/*fallback = TacheFeignClientFallback.class*/)
 public interface TacheFeignClient {
 
     @GetMapping("/taches/All/{projectId}")
+    @CircuitBreaker(name="TacheService", fallbackMethod = "getTaskFallback")
     ResponseEntity<List<Tache>> getAllTaches(@PathVariable("projectId") int projectId);
 
+    default ResponseEntity<List<Tache>> getTaskFallback(int projectId, Throwable exception) {
+        Tache tache = new Tache();
+        tache.setId((long) projectId);
+        tache.setDescription("Not available");
+        tache.setTitle("Not available");
 
+
+        return ResponseEntity.ok(Collections.singletonList(tache));
+    }
 
 }
