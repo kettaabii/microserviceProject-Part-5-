5d4a1f29af81e47868838f7e49a59093725c6ee1
kettaabii <elkettabimohammedamin@gmail.com>
2024-09-10 09:00:38 +0100

    hahahaha i added swagger config for api gateway

--- APiGateway/src/main/java/com/constructionExpert/APiGateway/APiGatewayApplication.java
+++ APiGateway/src/main/java/com/constructionExpert/APiGateway/APiGatewayApplication.java
@@ -1,5 +1,7 @@
 package com.constructionExpert.APiGateway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,11 +9,14 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class APiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -21,5 +26,16 @@ public class APiGatewayApplication {
 	@Bean
 	public DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rd, DiscoveryLocatorProperties dlp) {
 		return new DiscoveryClientRouteDefinitionLocator(rd, dlp);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/v3/api-docs").and().method(HttpMethod.GET).uri("lb://PROJECT-SERVICE"))
+				.route(r -> r.path("/v3/api-docs").and().method(HttpMethod.GET).uri("lb://TACHESERVICE"))
+				.route(r -> r.path("/v3/api-docs").and().method(HttpMethod.GET).uri("lb://RESSOURCE-SERVICE"))
+//				.route(r -> r.path("/v3/api-docs").and().method(HttpMethod.GET).uri("lb://USER-SERVICE"))
+				.build();
 	}
 }
