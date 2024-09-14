f01e49114cf15f46389620d9714dbdd6a50b3987
kettaabii <elkettabimohammedamin@gmail.com>
2024-09-14 20:46:06 +0100

    jwt token

--- APiGateway/src/main/java/Config/SecurityConfig.java
+++ APiGateway/src/main/java/Config/SecurityConfig.java
@@ -3,26 +3,27 @@ package Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                .authorizeExchange(exchanges->exchanges
-                        .pathMatchers("/login","/swagger-ui.html","/v3/api-docs").permitAll()
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/webjars/**").permitAll()
                         .anyExchange().authenticated()
-                ).oauth2Login(Customizer.withDefaults());
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
         return http.build();
-
-
-
-
     }
 }
